6adc00259c372e04d3dd3b184aa4d80297f19aea
JekaterinaCh <jekaterina.chvorostova@gmail.com>
2021-04-21 22:21:57 +0300

    TextView color change

--- app/src/main/java/com/example/fintech1lab2021/MainActivity.java
+++ app/src/main/java/com/example/fintech1lab2021/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.fintech1lab2021;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+    
     TextView tvMain;
+    private View button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OnBtnclick(View view) {
-        this.tvMain.setText(R.string.changed_label_capt);
+        this.tvMain.setText(R.string.changed_label_capt); }
+
+    public void OnBtnclickcolor(View view) {
+        TextView textElement = (TextView) findViewById(R.id.tvMain);
+        textElement.setTextColor(0xFF00FF00); //this is green color
     }
 }
